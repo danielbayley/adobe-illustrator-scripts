03c8a8081a5b0b885eab9a75d8c2c8387aa27edf
Daniel Bayley <daniel.bayley@me.com>
2025-05-17 19:07:45 +0100

    refactor: add skew utility

--- isometric/left.jsx
+++ isometric/left.jsx
@@ -1,9 +1,7 @@
-var selected = app.activeDocument.selection[0]
-var matrix   = app.getIdentityMatrix()
+#include "../../lib/utils.jsxinc"
 
 selected.height = selected.height / 100 * 86.062 // %
 
-matrix.mValueC = Math.tan(Math.PI / 180 * -30) // °
-selected.transform(matrix, true, true, true, true, 1)
+skew(selected, -30) // °
 
 selected.rotate(-30) // °
